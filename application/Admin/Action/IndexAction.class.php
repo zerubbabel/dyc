7e4ac3b6faf02582f4f6e8d6ae78c8d0df611461
zxxjjforever <zxxjjforever@163.com>
2014-10-30 07:30:34 +0800

    menu issues

--- application/Admin/Action/IndexAction.class.php
+++ application/Admin/Action/IndexAction.class.php
@@ -10,10 +10,10 @@ class IndexAction extends AdminbaseAction {
 	
 	function _initialize() {
 		parent::_initialize();
-		$this->initMenu();
 	}
     //后台框架首页
     public function index() {
+    	$this->initMenu();
         $this->assign("SUBMENU_CONFIG", json_encode(D("Menu")->menu_json()));
        	$this->display();
         
