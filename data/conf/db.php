0d31f9cedb82b500c99245c881f161e40426c129
zxxjjforever <zxxjjforever@163.com>
2014-10-30 07:50:36 +0800

    oschina.net params

--- data/conf/db.php
+++ data/conf/db.php
@@ -5,10 +5,10 @@
  */
 return array(
     'DB_TYPE' => 'mysql',
-    'DB_HOST' => MOPAAS_MYSQL15971_HOSTNAME,
-    'DB_NAME' => MOPAAS_MYSQL15971_NAME,
-    'DB_USER' => MOPAAS_MYSQL15971_USER,
-    'DB_PWD' => MOPAAS_MYSQL15971_PASSWORD,
+    'DB_HOST' => '10.4.12.173',
+    'DB_NAME' => 'db24ce9e0996443c49b78179a08cb19f9',
+    'DB_USER' => 'uZxjNfuFgLL40',
+    'DB_PWD' => 'p28rhn5QYkia6',
     'DB_PORT' => '3306',
     'DB_PREFIX' => 'sp_',
     //密钥
