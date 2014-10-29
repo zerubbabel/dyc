acd219998e81e14f7cd83013bf7233f71a20992e
zxxjjforever <zxxjjforever@163.com>
2014-10-30 07:47:45 +0800

    oschina params

---
+++ data/conf/db.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => MOPAAS_MYSQL15971_HOSTNAME,
+    'DB_NAME' => MOPAAS_MYSQL15971_NAME,
+    'DB_USER' => MOPAAS_MYSQL15971_USER,
+    'DB_PWD' => MOPAAS_MYSQL15971_PASSWORD,
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'sp_',
+    //密钥
+    "AUTHCODE" => 'euBpkBgE26BYMMMOwL',
+    //cookies
+    "COOKIE_PREFIX" => 'SDUgrK_',
+);
+?>
