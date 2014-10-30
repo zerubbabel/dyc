b4a116dee22f5e65502010ca51a4e127f14cc89e
zxxjjforever <zxxjjforever@163.com>
2014-10-30 10:49:36 +0800

    test

--- application/Admin/Action/TestAction.class.php
+++ application/Admin/Action/TestAction.class.php
@@ -14,9 +14,8 @@ class TestAction extends AdminbaseAction {
     //后台框架首页
     public function index() {
     	if( class_exists('\Common\Model\MenuModel')){
-    		echo "ddd";
+    		echo "exists";
     	}
-    	echo "1dd";
     	$class='\\Common\\Model\\MenuModel';
     	$menu_model=new $class();
     	$menu=$menu_model->menu_cache();
