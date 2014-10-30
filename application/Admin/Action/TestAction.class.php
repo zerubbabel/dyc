14b5469292714a570f3f970dc85c9ac97e0dac22
zxxjjforever <zxxjjforever@163.com>
2014-10-30 09:29:17 +0800

    test

---
+++ application/Admin/Action/TestAction.class.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * 后台首页
+ */
+namespace Admin\Action;
+use Common\Action\AdminbaseAction;
+class TestAction extends AdminbaseAction {
+	
+	
+	function _initialize() {
+		//parent::_initialize();
+	}
+    //后台框架首页
+    public function index() {
+    	
+    	$menu_model=new \Common\Model\MenuModel();
+    	$menu=$menu_model->menu_cache();
+    	print_r($menu);
+        
+    }
+
+    
+
+}
+
+?>
