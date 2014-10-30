0422f7726c0b00cf2581e127afa1121f466b3d2d
zxxjjforever <zxxjjforever@163.com>
2014-10-30 09:34:47 +0800

    d

--- application/Admin/Action/TestAction.class.php
+++ application/Admin/Action/TestAction.class.php
@@ -13,7 +13,9 @@ class TestAction extends AdminbaseAction {
 	}
     //后台框架首页
     public function index() {
-    	
+    	if( class_exists('\Common\Model\MenuModel')){
+    		echo "ddd";
+    	}
     	$menu_model=new \Common\Model\MenuModel();
     	$menu=$menu_model->menu_cache();
     	print_r($menu);
