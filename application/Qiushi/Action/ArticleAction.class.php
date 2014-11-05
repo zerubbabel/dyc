af7fed3fc5e1966215946d0f5314bdc90378bdcc
Yim <yeminch@qq.com>
2014-11-05 13:39:09 +0800

    设置赞和拍

--- application/Qiushi/Action/ArticleAction.class.php
+++ application/Qiushi/Action/ArticleAction.class.php
@@ -19,16 +19,19 @@ class ArticleAction extends HomeBaseAction {
 		$type=intval($_GET['like_type']);
 		if($type!=1) $type=-1;
 		
-		$can_set_like=sp_check_user_action("qiushi$id".$type,1);
+		$can_set_like=sp_check_user_action("qiushi$id",1);
 		if($can_set_like){
-			if($type==1)
+			if($type==1){
 				M("Qiushi")->save(array("id"=>$id,"like"=>array("exp","like+1")));
-			else
+				$this->success("您赞了一个！");
+			}else{
 				M("Qiushi")->save(array("id"=>$id,"unlike"=>array("exp","unlike+1")));
-    		$this->success("您赞了一个！");
+				$this->success("您拍了一板砖");
+			}
+    		
 		}else{
-			$this->success("您拍了一板砖");
-		};
+			$this->success("您已经点过了");
+		}
 	}
     
 }
