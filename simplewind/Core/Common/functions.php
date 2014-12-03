f3036096b7f85985cd3256cec8df86b3fe754647
zxxjjforever <zxxjjforever@163.com>
2014-12-03 18:35:45 +0800

    page issues

--- simplewind/Core/Common/functions.php
+++ simplewind/Core/Common/functions.php
@@ -549,11 +549,7 @@ function D($name='',$layer='') {
         }else{
             $class      =   '\\Common\\'.$layer.'\\'.$name.$layer;
         }
-        //$model      =   class_exists($class)? new $class($name) : new Think\Model($name);  //thinkcmf fix note
-        
-        //thinkcmf fix
-        $model      =   class_exists($class)? new $class() : new Think\Model($name);  //thinkcmf fix note
-        //thinkcmf fixed
+        $model      =   class_exists($class)? new $class($name) : new Think\Model($name);
     }else {
         Think\Log::record('D方法实例化没找到模型类'.$class,Think\Log::NOTICE);
         $model      =   new Think\Model(basename($name));
