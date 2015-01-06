291000e7be6a6bd3dd5072dd47a9349d60a44ffe
zxxjjforever <zxxjjforever@163.com>
2015-01-06 21:26:07 +0800

    ·修复入口文件SITE_PATH常量部分服务器异常

--- index.php
+++ index.php
@@ -21,17 +21,15 @@ if (ini_get('magic_quotes_gpc')) {
 //开启调试模式
 define("APP_DEBUG", true);
 //网站当前路径
-define('SITE_PATH', getcwd());
+define('SITE_PATH', "./");
 //项目路径，不可更改
-define('APP_PATH', SITE_PATH . '/application/');
+define('APP_PATH', SITE_PATH . 'application/');
 //项目相对路径，不可更改
-define('SPAPP_PATH',   SITE_PATH.'/simplewind/');
-/* //公共模块路径
-define('COMMON_PATH', "./".SPAPP_PATH ); */
+define('SPAPP_PATH',   SITE_PATH.'simplewind/');
 //
-define('SPAPP',   'application/');
+define('SPAPP',   './application/');
 //项目资源目录，不可更改
-define('SPSTATIC',   'statics/');
+define('SPSTATIC',   SITE_PATH.'statics/');
 //定义缓存存放路径
 define("RUNTIME_PATH", SITE_PATH . "/data/runtime/");
 //版本号
@@ -47,7 +45,6 @@ if(function_exists('saeAutoLoader') || isset($_SERVER['HTTP_BAE_ENV_APPID'])){
 		exit();
 	}
 }
-
 //uc client root
 define("UC_CLIENT_ROOT", './api/uc_client/');
 
@@ -56,7 +53,5 @@ if(file_exists(UC_CLIENT_ROOT."config.inc.php")){
 }
 
 //载入框架核心文件
-define('THINK_PATH',SPAPP_PATH.'Core/');
-define('ENGINE_NAME','cluster');
-require THINK_PATH.'ThinkPHP.php';
+require SPAPP_PATH.'Core/ThinkPHP.php';
 
