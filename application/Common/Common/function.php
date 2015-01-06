dd1567eb73370205e624e85969b2adfacb17d367
zxxjjforever <zxxjjforever@163.com>
2015-01-06 21:22:37 +0800

    ·移除SendMail方法 ·修复后台邮件发件人无法设置

--- application/Common/Common/function.php
+++ application/Common/Common/function.php
@@ -533,36 +533,6 @@ function sp_strip_chars($str, $chars='?<*.>\'\"'){
 	return preg_replace('/['.$chars.']/is', '', $str);
 }
 
-//发送邮件
-function SendMail($address,$title,$message){
-	import("PHPMailer");
-	$mail=new \PHPMailer();
-	// 设置PHPMailer使用SMTP服务器发送Email
-	$mail->IsSMTP();
-	$mail->IsHTML(true);
-	// 设置邮件的字符编码，若不指定，则为'UTF-8'
-	$mail->CharSet='UTF-8';
-	// 添加收件人地址，可以多次使用来添加多个收件人
-	$mail->AddAddress($address);
-	// 设置邮件正文
-	$mail->Body=$message;
-	// 设置邮件头的From字段。
-	$mail->From=C('SP_MAIL_ADDRESS');
-	// 设置发件人名字
-	$mail->FromName='ThinkCMF';
-	// 设置邮件标题
-	$mail->Subject=$title;
-	// 设置SMTP服务器。
-	$mail->Host=C('SP_MAIL_SMTP');
-	// 设置为"需要验证"
-	$mail->SMTPAuth=true;
-	// 设置用户名和密码。
-	$mail->Username=C('SP_MAIL_LOGINNAME');
-	$mail->Password=C('SP_MAIL_PASSWORD');
-	// 发送邮件。
-	return($mail->Send());
-}
-
 
 function sp_send_email($address,$subject,$message){
 	import("PHPMailer");
@@ -579,7 +549,7 @@ function sp_send_email($address,$subject,$message){
 	// 设置邮件头的From字段。
 	$mail->From=C('SP_MAIL_ADDRESS');
 	// 设置发件人名字
-	$mail->FromName='ThinkCMF';
+	$mail->FromName=C('SP_MAIL_SENDER');;
 	// 设置邮件标题
 	$mail->Subject=$subject;
 	// 设置SMTP服务器。
