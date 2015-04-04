fb67f796a7f5669eff9b056c55e3289add3a9f36
zxxjjforever <zxxjjforever@163.com>
2015-04-04 12:11:01 +0800

    删除无用代码

--- application/Api/Controller/CheckcodeController.class.php
+++ application/Api/Controller/CheckcodeController.class.php
@@ -13,7 +13,7 @@ class CheckcodeController extends Controller {
     	if (isset($_GET['length']) && intval($_GET['length'])){
     		$length = intval($_GET['length']);
     	}
-    		
+    	
     	//设置验证码字符库
     	$code_set="";
     	if(isset($_GET['charset'])){
@@ -46,10 +46,11 @@ class CheckcodeController extends Controller {
     		$height = intval($_GET['height']);
     	}
     	
-    	$background="";
+    	/* $background="";
     	if (isset($_GET['background']) && trim(urldecode($_GET['background'])) && preg_match('/(^#[a-z0-9]{6}$)/im', trim(urldecode($_GET['background'])))){
     		$background=trim(urldecode($_GET['background']));
-    	}
+    	} */
+    	//TODO ADD Backgroud param!
     	
     	$config = array(
 	        'codeSet'   =>  !empty($code_set)?$code_set:"2345678abcdefhijkmnpqrstuvwxyzABCDEFGHJKLMNPQRTUVWXY",             // 验证码字符集合
@@ -68,52 +69,6 @@ class CheckcodeController extends Controller {
     	$Verify->entry();
     }
     
-    public function test(){
-    	import("Checkcode");
-        $checkcode = new \Checkcode();
-        if (isset($_GET['code_len']) && intval($_GET['code_len']))
-            $checkcode->code_len = intval($_GET['code_len']);
-        if ($checkcode->code_len > 8 || $checkcode->code_len < 2) {
-            $checkcode->code_len = 4;
-        }
-        //设置验证码字符库
-        if(isset($_GET['charset'])){
-        	$checkcode->charset = trim($_GET['charset']);
-        }
-        //强制验证码不得小于4位
-        if($checkcode->code_len < 4){
-            $checkcode->code_len = 4;
-        }
-        if (isset($_GET['font_size']) && intval($_GET['font_size']))
-            $checkcode->font_size = intval($_GET['font_size']);
-        if (isset($_GET['width']) && intval($_GET['width']))
-            $checkcode->width = intval($_GET['width']);
-        if ($checkcode->width <= 0) {
-            $checkcode->width = 130;
-        }
-        if (isset($_GET['height']) && intval($_GET['height']))
-            $checkcode->height = intval($_GET['height']);
-        if ($checkcode->height <= 0) {
-            $checkcode->height = 50;
-        }
-        if (isset($_GET['font_color']) && trim(urldecode($_GET['font_color'])) && preg_match('/(^#[a-z0-9]{6}$)/im', trim(urldecode($_GET['font_color']))))
-            $checkcode->font_color = trim(urldecode($_GET['font_color']));
-        if (isset($_GET['background']) && trim(urldecode($_GET['background'])) && preg_match('/(^#[a-z0-9]{6}$)/im', trim(urldecode($_GET['background']))))
-            $checkcode->background = trim(urldecode($_GET['background']));
-        $checkcode->doimage();
-        
-        $verify = new \Think\Verify();
-        
-        //验证码类型
-        $type = I("get.type");
-        $type = $type?strtolower($type):"verify";
-        $verify = session("_verify_");
-        if(empty($verify)){
-            $verify = array();
-        }
-        $verify[$type] = $checkcode->get_code();
-        session("_verify_",$verify);
-    }
 
 }
 
