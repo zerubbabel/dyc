5d8179870a1fcdffb98ce07d24dd307c3aec2c9e
zxxjjforever <zxxjjforever@163.com>
2015-04-22 08:47:08 +0800

    更改静态缓存默认为关闭

--- application/Common/Conf/config.php
+++ application/Common/Conf/config.php
@@ -75,7 +75,7 @@ $configs= array(
 		
 		'OUTPUT_ENCODE'			=>true,// 页面压缩输出
 		
-		'HTML_CACHE_ON'     =>    true, // 开启静态缓存
+		'HTML_CACHE_ON'     =>    false, // 开启静态缓存
 		'HTML_CACHE_TIME'   =>    60,   // 全局静态缓存有效期（秒）
 		'HTML_FILE_SUFFIX'  =>    '.html', // 设置静态缓存文件后缀
 		
